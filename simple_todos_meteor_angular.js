4b264c5002f47a2edf0dbf259c7fe2b5a12b33e2
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-10-18 04:45:51 +0800

    Watch hideCompleted and change the query variable. Make query parameter reactive. Add incompleteCount to scope and template

--- simple_todos_meteor_angular.js
+++ simple_todos_meteor_angular.js
@@ -9,7 +9,10 @@ if (Meteor.isClient) {
  
       // Sort task list by date
       $scope.tasks = $meteor.collection(function() {
-        return Tasks.find({}, { sort: { createdAt: -1 } })
+        // To make Meteor understand Angular bindings and the other way around,
+        // we use `$scope.getReactively` function that turns Angular scope variables
+        // into Meteor reactive variables.
+        return Tasks.find($scope.getReactively('query'), { sort: { createdAt: -1 } });
       });
 
       $scope.addTask = function (newTask) {
@@ -17,6 +20,17 @@ if (Meteor.isClient) {
           text: newTask,
           createdAt: new Date() }
         );
+      };
+
+      $scope.$watch('hideCompleted', function() {
+        if ($scope.hideCompleted)
+          $scope.query = {checked: {$ne: true}};
+        else
+          $scope.query = {};
+      });
+
+      $scope.incompleteCount = function () {
+        return Tasks.find({ checked: {$ne: true} }).count();
       };
  
   }]);
