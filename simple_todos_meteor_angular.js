1f025092151b13c57555f914c81347437dba7906
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-10-18 05:29:55 +0800

    Publish only public and owner's tasks

--- simple_todos_meteor_angular.js
+++ simple_todos_meteor_angular.js
@@ -54,7 +54,12 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.publish('tasks', function () {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
   });
 }
 
